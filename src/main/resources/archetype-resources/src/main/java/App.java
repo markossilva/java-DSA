5c9b7ff63e275e44fcf941f609fd9f52f0469d7a
Markos da Silva <markosrsilva@gmail.com>
2023-11-12 14:35:53 -0300

    feature: initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $br.com.mks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
